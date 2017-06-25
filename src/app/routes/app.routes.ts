57cf7b96bd40f6297bdbfdd5109a85cd6a84c22b
Jeremy Cantu <mail@jeremycantu.com>
2017-06-25 10:31:22 -0500

    Fixed routing issues, pulling together existing components

--- src/app/routes/app.routes.ts
+++ src/app/routes/app.routes.ts
@@ -1,11 +1,10 @@
 import { Routes } from '@angular/router';
 
-//import { NavbarComponent } from '../components/navbar/navbar.component';
 import { AppComponent } from '../components/app/app.component';
+import { LandingComponent } from '../components/landing/landing.component';
 import { InteractionComponent } from '../components/interaction/interaction.component';
-//import { LandingComponent } from '../components/landing/landing.component';
 
 export const appRoutes: Routes = [
-    { path: '', component: AppComponent, pathMatch: 'full' },
+    { path: '', component: LandingComponent, pathMatch: 'full' },
     { path: 'interaction', component: InteractionComponent }
 ];

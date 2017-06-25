1c1185bb84cabbfc8c968a52cfeb60a92352deac
Jeremy Cantu <mail@jeremycantu.com>
2017-06-24 19:46:00 -0500

    Refactored out app component to landing component, deleted unecessary code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppComponent } from './app.component';
+import { LandingComponent } from './components/landing/landing.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    LandingComponent
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [LandingComponent]
 })
 export class AppModule { }
---
+++ src/app/components/landing/landing.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './landing.component.html',
+  styleUrls: ['./landing.component.css']
+})
+export class LandingComponent {
+  title = 'uKnowHealth';
+}

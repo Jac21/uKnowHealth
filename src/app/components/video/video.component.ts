64d8caa2b7267e68f535da5916cb97154aa12b79
Jeremy Cantu <mail@jeremycantu.com>
2017-06-25 11:45:04 -0500

    Re-added youtube video embed code

--- src/app/components/video/video.component.ts
+++ src/app/components/video/video.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-video',
@@ -6,8 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./video.component.css']
 })
 export class VideoComponent implements OnInit {
+  url: SafeResourceUrl;
+  video: any = {id: 'dQw4w9WgXcQ'};
+  baseUrl: string = 'https://www.youtube.com/embed/';
 
-  constructor() { }
+  constructor(private sanitizer: DomSanitizer) { 
+    this.url = this.sanitizer.bypassSecurityTrustResourceUrl(this.baseUrl + this.video.id);
+  }
 
   ngOnInit() {
   }

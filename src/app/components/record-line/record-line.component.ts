76b661ef706349b6a2abe09673c80948d7370778
Ivan Lopez <Ivan Lopez>
2017-06-25 14:23:32 -0500

    added selectors and some logic

--- src/app/components/record-line/record-line.component.ts
+++ src/app/components/record-line/record-line.component.ts
@@ -1,12 +1,20 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnChanges } from '@angular/core';
 import { RecordLine } from './record-line';
 @Component({
   selector: 'app-record-line',
   templateUrl: './record-line.component.html',
   styleUrls: ['./record-line.component.css']
 })
-export class RecordLineComponent {
+export class RecordLineComponent implements OnChanges {
+  
+
   @Input() record: RecordLine;
+  @Input() age: number;
+  @Input() gender: number;
+  private actualValue: number = 0;
+  ngOnChanges(changes): void {
+    this.actualValue = this.record.value + this.gender * 10 + this.age * 2.8;
+  }
 
   constructor() { }
 

aa7fd543ebb47ac693ca56464c36eac8e1367533
Ivan Lopez <Ivan Lopez>
2017-06-25 11:01:48 -0500

    Modified module and package.lock.json

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 import { appRoutes } from './routes/app.routes';
 
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {MdSliderModule} from '@angular/material';
-import {MdToolbarModule} from '@angular/material';
+import {MdSliderModule, MdToolbarModule, MdInputModule, MdTooltipModule} from '@angular/material';
 
 import 'hammerjs';
 
@@ -16,23 +16,28 @@ import { VideoComponent } from './components/video/video.component';
 import { ResourcesComponent } from './components/resources/resources.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { InteractionComponent } from './components/interaction/interaction.component';
+import { RecordGroupComponent } from './components/record-group/record-group.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
+    AppComponent, 
     SearchbarComponent,
     RecordLineComponent,
     VideoComponent,
     ResourcesComponent,
     NavbarComponent,
-    InteractionComponent
+    InteractionComponent,
+    RecordGroupComponent
   ],
   imports: [
     BrowserModule,
     RouterModule.forRoot(appRoutes),
     BrowserAnimationsModule,
+    FormsModule,
     MdSliderModule,
-    MdToolbarModule
+    MdToolbarModule,
+    MdInputModule,
+    MdTooltipModule
   ],
   providers: [],
   bootstrap: [AppComponent]
